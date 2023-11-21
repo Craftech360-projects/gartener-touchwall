aee959fdb86bae87705f2704a3fb2a99d928ed9d
rudresh-dev <rudresh@craftech360.com>
2023-11-21 13:08:05 +0530

    final push i think

--- src/app/dashboard/healthcare/result/page.jsx
+++ src/app/dashboard/healthcare/result/page.jsx
@@ -12,7 +12,7 @@ const page = () => {
       <div className="w-full h-full">
         <Image src={bg1} fill className="-z-10" />
       </div>
-      <div className=" w-[80vw] h-[70vh] flex justify-center items-center absolute top-[16%] left-[10%] rounded-[20px] border backdrop-blur-lg bg-white/30">
+      {/* <div className=" w-[80vw] h-[70vh] flex justify-center items-center absolute top-[16%] left-[10%] rounded-[20px] border backdrop-blur-lg bg-white/30">
         <div className="w-[50%] flex justify-center items-center flex-col">
           <div className="w-[600px] h-[300px]  rounded-2xl" style={{backgroundImage: `url(${caseStudy2})`, backgroundSize:`cover`}}>
           </div>
@@ -23,6 +23,27 @@ const page = () => {
           </div>
           <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/n-health-security.htm#collapse&pdf-overlay=//www.delltechnologies.com/asset/en-us/products/data-protection/customer-stories-case-studies/dell-technologies-n-health-customer-profile.pdf"} className="w-[600px] h-[300px] mt-5 text-white"><p className="text-[28px]">N Health modernizes its infrastructure on storage solutions from Dell Technologies to fortify security, provide caregivers with data to fuel decisions, deliver excellent patient experiences and drive regional expansion.</p></Link>
         </div>
+      </div> */}
+      <div className=" w-[80vw] h-[70vh] flex justify-center gap-32 items-center absolute top-[16%] left-[10%] rounded-[20px] border backdrop-blur-lg bg-white/30">
+        <div className="w-[17%] flex justify-center items-center flex-col">
+          <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/tgen.htm#collapse"} className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy2})`, backgroundSize:`cover`}}>
+          </Link>
+          <div className="w-[300px] h-[300px] mt-5 text-white"><p className="text-[20px]">TGen needed to be able to handle massive quantities of data in order to develop new treatments for
+patients.</p></div>
+        </div>
+        
+        <div className="w-[17%] flex justify-center items-center flex-col">
+          <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/bcb-medical-it-transformationc.htm#collapse&pdf-overlay=//www.delltechnologies.com/asset/en-us/solutions/apex/customer-stories-case-studies/dell-technologies-bcb-medical-customer-story.pdf"} className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy})`, backgroundSize:`cover`}}>
+          </Link>
+          <div className="w-[300px] h-[300px] mt-5 text-white"><p className="text-[20px]">BCB Medical is a Finnish healthcare software-as-a-service company gathering and analyzing clinical data. BCB Medical wanted a solution that is highly effective, provide access to cutting-edge IT and offer cloud-like simplicity and predictability for ease of planning.</p></div>
+        </div>
+
+        <div className="w-[17%] flex justify-center items-center flex-col">
+          <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/n-health-security.htm#collapse&pdf-overlay=//www.delltechnologies.com/asset/en-us/products/data-protection/customer-stories-case-studies/dell-technologies-n-health-customer-profile.pdf"} className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy})`, backgroundSize:`cover`}}>
+          </Link>
+          <div className="w-[300px] h-[300px] mt-5 text-white"><p className="text-[20px]">N Health modernizes its infrastructure on storage solutions from Dell Technologies to fortify security,provide caregivers with data to fuel decisions, deliver excellent patient experiences and drive regional expansion.</p></div>
+        </div>
+
       </div>
       <div className="absolute bottom-10 right-10 bg-yellow-400 px-10 py-3 rounded-3xl">
           <Link href={'/dashboard'} className="text-white font-semibold text-2xl">Home</Link>
--- src/app/dashboard/manufacturing/result/page.jsx
+++ src/app/dashboard/manufacturing/result/page.jsx
@@ -14,20 +14,20 @@ const page = () => {
       </div>
       <div className=" w-[80vw] h-[70vh] flex justify-center gap-32 items-center absolute top-[16%] left-[10%] rounded-[20px] border backdrop-blur-lg bg-white/30">
         <div className="w-[17%] flex justify-center items-center flex-col">
-          <div className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy2})`, backgroundSize:`cover`}}>
-          </div>
+          <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/linde-china-internet-of-things.htm#collapse&pdf-overlay=//www.delltechnologies.com/asset/en-us/solutions/infrastructure-solutions/customer-stories-case-studies/dell-technologies-linde-china-customer-profile.pdf"} className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy2})`, backgroundSize:`cover`}}>
+          </Link>
           <div className="w-[300px] h-[300px] mt-5 text-white"><p className="text-[20px]">To achieve efficiency, Linde wanted to develop a smart automated factory with Dell Technologies solutions, that would improve robotic automation and can meet increasing demand for electric forklifts while reduce energy use for itself and its customers.</p></div>
         </div>
-        
+
         <div className="w-[17%] flex justify-center items-center flex-col">
-          <div className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy})`, backgroundSize:`cover`}}>
-          </div>
+          <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/general-motors-high-performance-computing.htm#collapse"} className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy})`, backgroundSize:`cover`}}>
+          </Link>
           <div className="w-[300px] h-[300px] mt-5 text-white"><p className="text-[20px]">GM Motors wanted to create a future forward technology led environment that drives productivity, efficiency and increase in RoI with data driven, powerful, AI led applications.</p></div>
         </div>
 
         <div className="w-[17%] flex justify-center items-center flex-col">
-          <div className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy})`, backgroundSize:`cover`}}>
-          </div>
+          <Link href={"https://www.dell.com/en-us/dt/case-studies-customer-stories/hyundai-motor-group.htm#collapse"} className="w-[300px] h-[300px] rounded-2xl" style={{backgroundImage: `url(${caseStudy})`, backgroundSize:`cover`}}>
+          </Link>
           <div className="w-[300px] h-[300px] mt-5 text-white"><p className="text-[20px]">Hyundai Motor Group Innovation Center in Singapore wanted to build a human-centered, next generation smart manufacturing platform .</p></div>
         </div>
 
